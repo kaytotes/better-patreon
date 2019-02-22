9c4f356e9a9bd3913878d3e8021d959f08bacc33
Kaytotes <me@kaytotes.online>
2019-02-22 03:46:31 +0000

    Trim Frontend. Focus on Backend.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,10 +3,6 @@ import Vue from 'vue';
 
 import './generic'
 
-/* Login Component */
-import LoginComponent from './components/Auth/Login.vue';
-Vue.component('login', LoginComponent);
-
 /* Instantiate Vue */
 new Vue({
     el: '#app'
