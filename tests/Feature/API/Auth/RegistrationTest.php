8533e2be250b6e3bf8fa58393c47fb4899cab80c
Kaytotes <me@kaytotes.online>
2019-02-22 07:36:40 +0000

    Name validation test

--- tests/Feature/API/Auth/RegistrationTest.php
+++ tests/Feature/API/Auth/RegistrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\API\Auth;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
@@ -38,7 +39,7 @@ class RegistrationTest extends TestCase
         $response = $this->json('POST', route('api.register'), $data);
 
         /* Success Response */
-        $response->assertStatus(200);
+        $response->assertStatus(Response::HTTP_OK);
 
         /* Check Json */
         $response->assertJsonStructure([
@@ -59,5 +60,41 @@ class RegistrationTest extends TestCase
         Event::assertDispatched(Registered::class, function ($e) use ($user) {
             return $e->user->id === $user->id;
         });
+    }
+
+    /**
+     * Tests that the Name is required and can't be too long.
+     *
+     */
+    public function testNameValidation()
+    {
+        /* Base Data */
+        $data = [
+            'email' => 'test@test.test',
+            'password' => 'testing',
+            'password_confirmation' => 'testing',
+        ];
+
+        /* No Name Provided */
+        $response = $this->json('POST', route('api.register'), $data);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonValidationErrors([
+            'name',
+        ]);
+
+        /* Name Too Long */
+        $data = array_merge($data, [
+            'name' => str_random(256),
+        ]);
+        $response = $this->json('POST', route('api.register'), $data);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonValidationErrors([
+            'name',
+        ]);
+    }
+
+    public function testEmailValidation()
+    {
+        $this->assertTrue(false);
     }
 }
