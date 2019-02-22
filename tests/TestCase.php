fae50add521626dfccda001795b83bf9f879d9b6
Kaytotes <me@kaytotes.online>
2019-02-22 04:46:09 +0000

    Added a testing helper function for setting up passport.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Sets up Laravel Passport.
+     *
+     */
+    public function setUpPassport()
+    {
+        \Artisan::call('passport:install', ['-vvv' => true]);
+    }
 }
